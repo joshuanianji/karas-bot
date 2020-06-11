11407e96eaafc303c05ef9b689a6f8379ea7b5d0
Alexander Cai <alexandercai@outlook.com>
2020-06-11 16:24:46 -0600

    minor updates

--- bot.js
+++ bot.js
@@ -1,8 +1,13 @@
 const Discord = require('discord.js');
+// This is an example of 
 const { prefix, token } = require('./config.json');
 
 const client = new Discord.Client();
 
+// Intro to callbacks:
+// Essentially what this code is doing is that once it receives
+// the "ready" event, it will run the second argument passed, which *is* a function.
+// Don't worry if this seems confusing — treating functions as first-class objects can be a tricky concept! We'll talk about it in one of our meetings.
 client.once('ready', () => {
     console.log('Ready!');
 });
@@ -14,14 +19,15 @@ client.on('message', message => {
     // we ignore it
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    // gets everything after the "!" 
+    // gets everything after the ";"
     // and splits off everything else into spaces
-    //e .g. !help one two => ["help", "one", "two"]
+    //e .g. ;help one two => ["help", "one", "two"]
     // we use prefix.length because we might change the prefix (located in config.json) into something else
     const args = message.content.slice(prefix.length).split(/ +/);
-    // makes everything lowercase and also takes off the first element and stores that into the command variable.
-    const command = args.shift().toLowerCase();
 
+    // Array.prototype.shift() pops off the first element of the array and returns it.
+    // String.prototype.toLowerCase() makes the string all lowercase.
+    const command = args.shift().toLowerCase();
 
     // there aren't many commands so I used an if/else statement, but there are better ways once your list of commands grows!
     if (command === 'goodmorning') {
@@ -38,7 +44,7 @@ client.on('message', message => {
                 .setImage('https://imgur.com/slphnBI.jpg');
 
 
-        message.channel.send(pictureEmbed)
+        message.channel.send(pictureEmbed);
 
     } else if (command === 'help') {
         message.channel.send('Stop playing around with the bot and go code.');
