f1604bddced33ccf6e9bae8a9d351d453ab5c191
Joshua Ji <joshuanianji@users.noreply.github.com>
2020-06-11 22:53:42 -0600

    Slight changes to make it sound better

--- bot.js
+++ bot.js
@@ -73,8 +73,9 @@ client.on('guildMemberAdd', async (member) => {
         .setThumbnail(osacsLogo)
 
       DMCHannel.send(embedMsg);
-      DMCHannel.send("**Please read and respect the rules written in `#README` channel**, and if you have any questions, feel free to message anyone with the `exec` role. Failing to follow the rules can result in a ban - or even a harsh word from me!");
-      DMCHannel.send("I am an omnipotent deity: the digital incarnation of Mr. Karas, the Computer Science teacher at OSA. I'm still a work in progress, and if you have any suggestions for things I can do, we would **love** to hear your thoughts in the `#suggestions` channel. And if you want to make your own Discord bot, we'd love that as well! There is an `OSACS Certified Bots` role just for this kind of thing. Happy coding!");
+      DMCHannel.send(`Welcome to the OSACS Discord, ${member.user.username}`);
+      DMCHannel.send("To start off, **Please read and respect the rules written in `#README` channel**. If you have any questions, feel free to message anyone with the `exec` role. Failing to follow the rules can result in a ban - or even a harsh word from me!");
+      DMCHannel.send("A bit about me: I am an omnipotent deity: the digital incarnation of Mr. Karas, the Computer Science teacher at OSA. I'm still a work in progress, and if you have any suggestions for things I can do, we would **love** to hear your thoughts in the `#suggestions` channel. And if you want to make your own Discord bot, we'd love to add it to our server! There is an `OSACS Certified Bots` role just for this kind of thing. Happy coding!");
 
       console.log(`Successfully sent welcome message to: ${member.user.username}`)
     })
